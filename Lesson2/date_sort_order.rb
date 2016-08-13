040ed3de48ff59b5e2b1ba0c896869ea2a267109
perkir <kspersionov@gmail.com>
2016-08-14 01:52:57 +0300

    lesson2 step 4

---
+++ Lesson2/date_sort_order.rb
@@ -0,0 +1,17 @@
+print "Введите число, месяц, год "
+number = gets.chomp.to_i
+month = gets.chomp.to_i - 1
+year = gets.chomp.to_i
+is_leap_year = 0
+is_leap_year = 1 if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+order_days = 0
+
+for order_month in 1..month
+  order_month -= 1
+  order_days += is_leap_year if order_month ==  1
+  order_days += days_in_month[order_month]
+end
+
+puts "Порядковый номер даты: #{order_days += number}"
+
