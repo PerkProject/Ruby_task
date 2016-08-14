19d6738b2e17f480bcff3b4e28eebeb2282d9a86
perkir <kspersionov@gmail.com>
2016-08-14 22:16:07 +0300

    refactor

--- Lesson2/hash_letter.rb
+++ Lesson2/hash_letter.rb
@@ -2,9 +2,8 @@ alfabet = ("a".."z")
 vowels = ["a", "e", "i", "o", "u", "y"]
 vowels_index = {}
 sort_number = 0
-alfabet.each_with_index do |letter|
-  sort_number += 1
-  vowels_index[letter] = sort_number if vowels.include?(letter)
+alfabet.each_with_index do |letter, index|
+  vowels_index[letter] = index + 1 if vowels.include?(letter)
 end
 
 puts vowels_index
