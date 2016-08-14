d699c76ef3fb549360187a8c49b0cd44122ef993
perkir <kspersionov@gmail.com>
2016-08-14 17:05:41 +0300

    refactor

--- Lesson1/quadratic_equation.rb
+++ Lesson1/quadratic_equation.rb
@@ -1,10 +1,13 @@
 print "Введите 3 коэффициента: "
 a = gets.chomp.to_f
+a = 0.0 if a.nil? == true  || a.nan? == true
 b = gets.chomp.to_f
+b = 0.0 if b.nil? == true  || b.nan? == true
 c = gets.chomp.to_f
+c = 0.0 if c.nil? == true  || c.nan? == true
 d = b ** 2 - 4 * a * c
 
-if d > 0 
+if d > 0
 	puts "Дискриминант: #{d}"
 	x1 = (-b + Math.sqrt(d)) / a * 2
 	puts "Первый корень: #{x1}"
@@ -14,7 +17,7 @@ elsif d == 0
  	puts "Дискриминант: #{d}"
 	x1 = (-b + Math.sqrt(d)) / a * 2
 	puts "Первый корень: #{x1}"
-elsif d < 0 
+else
     puts "Дискриминант: #{d}"
-    puts "Корней нет "	
-end     
+    puts "Корней нет "
+end
