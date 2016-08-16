dbbe193e3c5b827393182394fc0e2bb7e3595e6b
perkir <kspersionov@gmail.com>
2016-08-16 23:42:54 +0300

    bad realisation

--- Lesson3/station.rb
+++ Lesson3/station.rb
@@ -14,6 +14,14 @@ class Station
    @trains.join("\n")
  end
 
+ def show_trains_by_type(type)
+  show_trains_by_type = []
+  self.trains.each do |train|
+    show_trains_by_type << train if train.type == type
+  end
+  puts "С типом #{type} #{trains_by_type.size} поезд/а/ов"
+ end
+
  def send_train(name)
    puts "Поезд уже уехал" unless @trains.include? name
    @trains.delete(name)
--- Lesson3/train.rb
+++ Lesson3/train.rb
@@ -1,16 +1,27 @@
 class Train
   TYPE = [:"Грузовой", :"Пассижирский"].freeze
-  attr_accessor :speed
-  attr_reader :carriage_qty
+  attr_accessor :route
+  attr_reader :carriage_qty, :type, :speed
 
   def initialize (number, type, carriage_qty)
     @number = number
     @type = type
     @carriage_qty = carriage_qty
+    @cur_station = nil
+    @route = nil
+    @prev_station = nil
+  end
+
+  def go_speed(speed)
+    @speed += speed
   end
 
   def stop
     @speed = 0
+  end
+
+  def show_carrige_qty
+    @carriage_qty
   end
 
   def add_carriage
@@ -21,5 +32,25 @@ class Train
     self.carriage_qty -= 1 if @speed == 0 && self.carriage_qty > 0
   end
 
+  def take_route(route)
+    @route = route
+    @cur_station = @start_station
+    self.station.take_train(self)
+  end
+
+private
+
+  def go_to_next_station(name)
+    @prev_station = @cur_station
+    station = name
+    @cur_station = name
+    station.take_train(self)
+    station.send_train(self)
+  end
+
+  def send_from_prev_station
+    station = @cur_station
+    station.send_train(self)
+  end
 
 end
